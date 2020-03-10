3b88bdb53c263ccbca7d7ede9f359aa8f90482f0
Rob Condon <rtc33@uclive.ac.nz>
2020-03-11 01:50:47 +1300

    Changed local host to

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ module.exports ={
         user: process.env.DB_USER || 'ACC',
         password: process.env.DB_PASS || 'password',
         database: process.env.DB_NAME || 'ACCBlog',
-        host: process.env.HOST || 'localhost',
+        host: process.env.HOST || '127.0.0.1',
         dialect: 'sqlite',
         storage: './AmericanCarClub.sqlite'
     }
