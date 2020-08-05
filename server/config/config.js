7b4cb18c1c3a183c7e84c0bbe2a7367264f0c29a
Rob Condon <rtc33@uclive.ac.nz>
2020-08-05 22:39:50 +1200

    Try again

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ module.exports ={
         user: process.env.DB_USER || 'ACC',
         password: process.env.DB_PASS || 'password',
         database: process.env.DB_NAME || 'ACCBlog',
-        host:  'http://138.68.5.128' || '127.0.0.1',
+        host:  'http://138.68.18.100' || '127.0.0.1',
         dialect: 'sqlite',
         storage: './AmericanCarClub.sqlite'
     }
