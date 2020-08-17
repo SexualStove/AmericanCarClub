c029243fe95aa7cc75aa6bfef8f97f0d528a991b
Rob Condon <rtc33@uclive.ac.nz>
2020-08-17 22:23:14 +1200

    Change baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: 'http://localhost:8081'
-        //|| 'http://138.68.18.100:8081'
+        //baseURL: 'http://localhost:8081'
+        baseURL: 'http://138.68.18.100:8081'
     });
 
 }
