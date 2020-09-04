4b1a967e52ea9ecaf27533c03f050373b8aac8be
Rob Condon <rtc33@uclive.ac.nz>
2020-09-04 15:08:48 +1200

    Added one key word

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BaseURL = 'http://138.68.18.100:8081';
+export const BaseURL = 'http://138.68.18.100:8081';
 //export const BaseURL = 'http://localhost:8081';
 export default () => {
 
