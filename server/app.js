16316360adf24dcc7781d1fa03b378222557b173
Rob Condon <rtc33@uclive.ac.nz>
2020-09-28 18:59:43 +1300

    Changed to https and inhouswebtest.site

--- server/app.js
+++ server/app.js
@@ -4,23 +4,44 @@ const cors = require('cors');
 const morgan = require('morgan');
 const {sequelize} = require('./models');
 const config = require('./config/config');
+const exphbs = require('express-handlebars');
 
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
+const https = require("https"),
+    fs = require("fs");
 
+const options = {
+    key: fs.readFileSync("/etc/letsencrypt/live/inhousewebtest.site/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/inhousewebtest.site/fullchain.pem")
+};
 
+//View Engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 require('./routes')(app);
 
 sequelize.sync()
     .then(() => {
 
-        app.listen(process.env.PORT || 8081);
+        https.createServer(options,app).listen(process.env.PORT || 8081);
         console.log(`Server started on port ${config.port}`)
 
     });
+
+console.log("Listening");
+
+
+
+
+
+
+//app.listen(process.env.PORT || 8081);
+
+
 
 console.log("Listening");
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-export const BaseURL = 'http://138.68.18.100:8081';
+//export const BaseURL = 'http://138.68.18.100:8081';
 //export const BaseURL = 'http://localhost:8081';
+export const BaseURL = 'https://inhousewebtest.site:8081';
 export default () => {
 
     return axios.create({
